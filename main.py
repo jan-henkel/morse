d544c2c6f0c3c238eac90e0e5dfa74c36892d772
Jan Henkel <jan.pascal.henkel@googlemail.com>
2021-04-11 17:16:04 +0200

    Add audio decoding option in main script

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from morse import morse_text, morse_audio
+from morse import morse_text, morse_audio, morse_audio_decode
 
 if __name__ == "__main__":
-    print("Enter 1 to encode, 2 to decode, 3 to encode and generate audio: ")
+    print(
+        "Enter 1 to encode, 2 to decode, 3 to encode and generate audio, 4 to decode audio: "
+    )
     mode = int(input())
     if mode == 1:
         print("Enter text: ")
@@ -15,3 +17,7 @@ if __name__ == "__main__":
         print("Enter text: ")
         text = input()
         morse_audio.morse_beep(text)
+    elif mode == 4:
+        print("Enter filename: ")
+        filename = input()
+        print(morse_audio_decode.soundfile_to_text(filename))
